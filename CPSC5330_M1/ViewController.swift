92bdaa53ab0a8697243626048e94666fea4cdc37
Nikki U <nunvericht@gmail.com>
2024-01-12 20:40:18 -0600

    Initial Commit

---
+++ CPSC5330_M1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC5330_M1
+//
+//  Created by Nicholl Unvericht on 1/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
